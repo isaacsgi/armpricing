eb8d73e861ddb66144a253332b2b1bdbc2768518
Cale Teeter <caleteeter@hotmail.com>
2016-01-28 11:16:17 -0800

    added upload button

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* UPLOAD */
+router.post('/upload', function(req, res) {
+	console.dir(req.files);
+});
+
 module.exports = router;
