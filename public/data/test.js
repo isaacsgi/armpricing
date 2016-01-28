7e4e3b7b0678d5e1fd402f8bddf00db54a15206f
Cale Teeter <caleteeter@hotmail.com>
2016-01-28 14:15:11 -0800

    updated parsing hack code

---
+++ public/data/test.js
@@ -0,0 +1,11 @@
+{
+	"offers": {
+		"basic-a0": {
+				"cores":1,
+				"ram":0.75,
+				"diskSize":20,
+				"diskType":"HDD",
+				"linuxSupportPrice":0.1
+			}
+		}
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var multer = require("multer");
+var multer = require('multer');
+var path = require('path');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,6 +17,17 @@ router.post('/',[ multer({ dest: './uploads/'}), function(req, res){
 
 router.post('/', function (req, res){
 	console.dir(req.files);
+});
+
+router.post('/parse', function(req, res){
+	// parse the pricing (will make this dynamic and not suck in the future)
+	var pricing_json = JSON.parse(fs.readFileSync(path.join(__dirname, '../public/data/Virtual-Machines.js'), 'utf-8'));
+	debugger; 
+	
+	var core = pricing_json.offers['basic-a0']['prices'];
+	console.log(core['us-east-windows']);
+	
+	res.render('index', { title: 'test' });
 });
 
 module.exports = router;
