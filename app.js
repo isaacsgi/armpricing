8472d73858c842935523abddc56bd42ef9d448c5
Cale Teeter <caleteeter@hotmail.com>
2016-01-28 11:27:41 -0800

    added routes for upload

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(multer({dest:'/uploads/'}));
+var upload = multer({ dest: '/uploads/' });
 
 app.use('/', routes);
 app.use('/users', users);
